83b00dc812da33ddf932add4bd0b765be1cd1c89
Filipe Melo <filipe.melo@liferay.com>
2021-05-21 15:44:26 -0300

    Service Builder

---
+++ h7g5-service/src/main/java/com/liferay/h7g5/model/impl/H7G5EntryImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.h7g5.model.impl;
+
+/**
+ * The extended model implementation for the H7G5Entry service. Represents a row in the &quot;OHQIWTSFHL_H7G5Entry&quot; database table, with each column mapped to a property of this class.
+ *
+ * <p>
+ * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.h7g5.model.H7G5Entry</code> interface.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class H7G5EntryImpl extends H7G5EntryBaseImpl {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. All methods that expect a h7g5 entry model instance should use the {@link com.liferay.h7g5.model.H7G5Entry} interface instead.
+	 */
+	public H7G5EntryImpl() {
+	}
+
+}
---
+++ h7g5-service/src/main/java/com/liferay/h7g5/service/impl/H7G5EntryServiceImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.h7g5.service.impl;
+
+import com.liferay.h7g5.service.base.H7G5EntryServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The implementation of the h7g5 entry remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.h7g5.service.H7G5EntryService</code> interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see H7G5EntryServiceBaseImpl
+ */
+@Component(
+	property = {
+		"json.web.service.context.name=ohqiwtsfhl",
+		"json.web.service.context.path=H7G5Entry"
+	},
+	service = AopService.class
+)
+public class H7G5EntryServiceImpl extends H7G5EntryServiceBaseImpl {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use <code>com.liferay.h7g5.service.H7G5EntryServiceUtil</code> to access the h7g5 entry remote service.
+	 */
+}
